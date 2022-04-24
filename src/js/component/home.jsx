bc741305464adc1e6aa273e601dedb002b73f4c4
Navarroseb <sebastnavarro@gmail.com>
2022-04-24 23:18:25 +0000

    debugging fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [newItem, setNewItem] = useState("");
+	const [todo, setTodo] = useState([]);
+
+	useEffect(() => {
+		getTodo();
+	}, [newItem]);
+
+	const sendTodo = async (result) => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/navarroseb",
+			{
+				method: "POST",
+				body: JSON.stringify(result),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+	};
+	const getTodo = async () => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/navarroseb",
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+		setTodo(data);
+	};
+
+	const deleteItem = (id) => {
+		const newArray = todo.filter((element, item) => item !== id);
+		setTodo(newArray);
+		sendTodo(newArray);
+	};
+
+	const addTodo = (e) => {
+		if (newItem === "") {
+			return;
+		}
+		if (e.key === "Enter") {
+			const task = {
+				label: newItem,
+				done: false,
+			};
+			sendTodo([...todo, task]);
+			setNewItem("");
+		}
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main-container">
+			<h1 className="text-center mt-5">TO DO LIST</h1>
+			<div className="card-of-list">
+				<input
+					type="text"
+					className="add-todo"
+					placeholder="Add a new to do..."
+					onChange={(e) => {
+						setNewItem(e.target.value);
+					}}
+					value={newItem}
+					onKeyPress={(e) => addTodo(e)}
+				/>
+				<ul className="group-list">
+					{Array.isArray(todo) &&
+						todo !== undefined &&
+						todo?.map((task, i) => {
+							return (
+								<li className="items" key={i}>
+									{task.label}
+									<button
+										className="delete-item"
+										onClick={() => {
+											deleteItem(i);
+										}}>
+										<i class="fas fa-trash-alt"></i>
+									</button>
+								</li>
+							);
+						})}
+				</ul>
+				<h5>{`${
+					Array.isArray(todo) && todo !== undefined && todo.length
+				} items left`}</h5>
+			</div>
 		</div>
 	);
 };
